bb23546abdd2a0cdda83e0b879b847a75fca394b
Егор Власенко <evlasenko@iteq.ru>
2011-08-12 14:44:11 +0400

    start git repo'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErnilHomepage::Application.config.secret_token = '9207081ea46a46074e42f11d900291f00d2e53b99139412d4314ac58d993d66c66d12a8377d5fbc9bfea4cac711c270335c55dcc0588a0c58bdeaa4dfa4d4a90'
